8d6d43f393f87776544c74cd2b3c7d49774b9eca
gureenkov56 <gureenkov56@gmail.com>
2021-11-28 20:27:01 +0300

    Animate for mobile menu x

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const headerMobileMenuToggler = document.getElementById('headerMobileMenuToggler'),
         headerMenuWrapper = document.getElementById('header__menu-wrapper'),
-        menuMain = document.getElementById('headerMenu');
+        menuMain = document.getElementById('headerMenu'),
+        burgerTop = document.querySelector('.burger-top'),
+        burgerMiddle = document.querySelector('.burger-middle'),
+        burgerBottom = document.querySelector('.burger-bottom');
+
 
     // open and close mobile menu
     headerMobileMenuToggler.addEventListener('click', () => {
         if(headerMenuWrapper.dataset.open === 'true'){
             headerMenuWrapper.dataset.open = 'false';
             headerMenuWrapper.style.height = 0;
+            burgerTop.style.opacity = '1';
+            burgerMiddle.style.cssText = 'transform: rotate(0deg);';
+            burgerBottom.style.cssText = 'margin-top: 0px; transform: rotate(0deg);';
         } else {
             headerMenuWrapper.dataset.open = 'true';
             headerMenuWrapper.style.height = menuMain.scrollHeight + 'px';
+            burgerTop.style.opacity = '0';
+            burgerMiddle.style.cssText = 'transform: rotate(45deg);';
+            burgerBottom.style.cssText = 'margin-top: -7px; transform: rotate(-45deg);';
         }
     })
 
